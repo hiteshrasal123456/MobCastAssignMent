50700e8382776b8248184326f347c3b68a5fdc0e
apple <apple@apples-MacBook-Air-2.local>
2018-02-17 19:32:00 +0530

    Initial Commit

---
+++ MobCastAssignMent/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobCastAssignMent
+//
+//  Created by apple on 17/02/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MobCastAssignMent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MobCastAssignMent
+//
+//  Created by apple on 17/02/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
